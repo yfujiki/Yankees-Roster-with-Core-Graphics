5649b84956268f0a29ac84870d975646c82ad75a
Yuichi Fujiki <yuichi@chaione.com>
2012-10-06 00:10:21 -0500

    Initial Commit

---
+++ BaseBallPark/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaseBallPark
+//
+//  Created by Yuichi Fujiki on 10/6/12.
+//  Copyright (c) 2012 Yuichi Fujiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaseBallPark/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BaseBallPark
+//
+//  Created by Yuichi Fujiki on 10/6/12.
+//  Copyright (c) 2012 Yuichi Fujiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
